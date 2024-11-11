c1ff121c8b96ad6a41cd9e780a510632d442a51c
aayush-navsariwala <aayush.nav@gmail.com>
2024-11-11 10:38:12 +0200

    Commit 1 - Added attendance.css

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace XBCAD7319_SparkLine_HR_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
