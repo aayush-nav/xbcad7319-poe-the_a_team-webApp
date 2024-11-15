fe6325e59334856a4ba8436bdada305836f89a2f
AslamK786 <st10104776@vcconnect.edu.za>
2024-11-15 18:24:03 +0200

    Aslam Added Training on Performance

--- Models/PerformanceReview.cs
+++ Models/PerformanceReview.cs
@@ -12,4 +12,13 @@ namespace XBCAD7319_SparkLine_HR_WebApp.Models
         // List to hold past reviews
         public static List<PerformanceReview> PastReviews { get; set; } = new List<PerformanceReview>();
     }
+
+    public class Training
+    {
+        public string EmployeeName { get; set; }
+        public string CourseName { get; set; }
+        public string CourseLink { get; set; }
+        public DateTime CompletionDate { get; set; }
+        public bool Status { get; set; } // "Complete" or "Incomplete"
+    }
 }
--- obj/Debug/net8.0/XBCAD7319-SparkLine-HR-WebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/XBCAD7319-SparkLine-HR-WebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XBCAD7319-SparkLine-HR-WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb15bdbfd442791e9ad102a838f6b71ba5233887")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e63c2404e25e5be164ad4cf74024b01d30d47bf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("XBCAD7319-SparkLine-HR-WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XBCAD7319-SparkLine-HR-WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
